e664203fbb1b780c7daa9170b5c23d147e5446ac
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-04-02 01:55:14 +0300

    feat: update comment for polyfill

--- method.js
+++ method.js
@@ -1,4 +1,6 @@
 // Polyfill , Полифил
+// Этот метод был добавлен к спецификации ECMAScript 6 и может быть доступен ещё не во всех реализациях JavaScript. 
+// Однако, вы можете легко эмулировать этот метод при помощи следующего кода:
 if (!String.prototype.endsWith) {
     Object.defineProperty(String.prototype, 'endsWith', {
       value: function(searchString, position) {
